97cb2f7d906c17e983845f482f2d0cc4e958aa69
Pınar <pinarsuvac@gmail.com>
2020-12-15 17:58:34 +0300

    ID create function builded

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+/* Create unique ID */
+const IDcreator = require(__dirname + "/createID.js");
+
+app.use(cors());
+
+app.use(express.static('public'));
+
+/* Person object */
+const newPerson = {
+  username : String,
+  _id: String
+}
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post("/api/exercise/new-user", (req,res)=>{
 
+
+  
+});
 
 
 
