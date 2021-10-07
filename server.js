d1af518ae87c54d815382f5aa1b393058b27e1eb
Pınar <pinarsuvac@gmail.com>
2021-10-08 01:23:39 +0300

    excersize tracker initial code added

--- server.js
+++ server.js
@@ -1,200 +1,154 @@
 const express = require('express');
-const mongo = require("mongodb");
-const mongoose = require("mongoose");
 const app = express();
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const moment = require('moment');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const moment = require("moment");
 require('dotenv').config();
-
-
-const bodyParser = require("body-parser");
-
-/* Create unique ID */
-const IDcreator = require(__dirname + "/createID.js");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
-/* Database connections */
-mongoose.connect(process.env.MONGO_URI,{useUnifiedTopology:true, useNewUrlParser:true});
+/* Database Connection */
+mongoose.connect(process.env['Mongo_URI'], {useUnifiedTopology: true, useNewUrlParser: true});
 const connection = mongoose.connection;
-connection.on("error",console.log.bind(console,"connection error:"));
+connection.on("error", console.log.bind(console, "connection error:"));
 
-connection.once("open",()=>{
-console.log("MongoDB database connection established successfully");
+connection.once("open", () => {
+	console.log("Mongodb database connection established successfully");
 });
 
 /* Schema */
-const exerciseData = new mongoose.Schema({
-  description: {type: String, required: true},
-  duration: {type: Number, required: true},
-  date: {
-    type: Date,
-    default: Date.now()  
-  }
- 
+const excersizeSchema = new mongoose.Schema({
+	description: {type: String, required: true},
+	duration: {type: Number, required: true},
+	date: {type: String}
 });
 
 const userSchema = new mongoose.Schema({
-  username : {type: String, required: true},
-  _id: {type: String, required: true},
-  log:[exerciseData]
+	username: {type: String, required: true},
+	log: [excersizeSchema]
 });
 
 /* Model */
 const User = mongoose.model("User", userSchema);
 
+/* Create new user */
+app.post('/api/users', (req, res)=>{
+	const userName = req.body.username;
+
+	User.findOne({username: userName}, (err, result) => {
+		if (err) throw err;
+		if(!result) {
+			const user = new User({
+				username: userName
+			});
+			user.save();
+			res.json(user);
+		} else {
+			// Message for user is already saved
+			res.json("Username already taken");
+		}
+	});
+});
+
+/* create user log data */
+app.post('/api/users/:_id/exercises', (req, res)=>{
+	let date = req.body.date ? moment(req.body.date).format("ddd MMM DD YYYY") : moment().format("ddd MMM DD YYYY");
+
+	const user_id = req.params._id;
+	const user_desc = req.body.description;
+	const user_dura = parseInt(req.body.duration);
+
+	User.findOne({_id: user_id}, (err,result)=>{
+		if (err) throw err;
+		if(!result) {
+			res.json("The user you were looking for was not found, check your user ID");
+		} else {
+			result.log.push({
+				description: user_desc,
+				duration: user_dura,
+				date: date,
+			});
+			result.save();
+			res.json(
+				{
+					username: result.username,
+					description: user_desc,
+					duration: user_dura,
+					date: date,
+					_id: result._id
+				}
+			);
+		}
+	});
+	
+});
+
+/* Retrive user log */
+app.get('/api/users/:_id/logs', (req,res)=>{
+
+	const user_id = req.params._id;
+
+	const from_date = req.query.from;
+	const to_date = req.query.from;
+	const limit = req.query.limit;
+
+	let start_date =  moment(from_date).format("YYYY-MM-DD");
+	let end_date = moment(to_date).format("YYYY-MM-DD");
+
+	User.findOne({_id: user_id}, (err,result)=>{
+		if(!result) {
+			res.json("The user you were looking for was not found, check your user ID");
+		} else {
+			if(from_date && to_date) {
+				let filteredItems = result.log.filter((item) => {
+					let date = new Date(item.date);
+					let itemdate = moment(date).format("YYYY-MM-DD");
+					//todo: bug here -> moment(itemdate).isBetween(start_date,end_date)
+					return moment(itemdate).isBetween('2021-01-11','2021-02-13');
+				});
+				console.log(filteredItems);
+				res.json(
+					{
+						username: result.username,
+						count: result.log.length,
+						_id: result._id,
+						log: filteredItems,
+					}
+				);
+			} else {
+				res.json(
+					{
+						username: result.username,
+						count: result.log.length,
+						_id: result._id,
+						log: result.log,
+					}
+				);
+			}
+		};
+	});
+});
+
+/* List all users */
+app.get('/api/users', (req, res) => {
+  User.find({}, (err,users)=>{
+		if (err) throw err;
+		res.json(users);
+	});
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-// Show all users with exercise infos
-app.get("/api/exercise/users",(req,res)=>{
-  User.find({},(err,results)=>{
-    res.json(results);
-  });
-});
 
-app.post("/api/exercise/new-user", (req,res)=>{
-const userName = req.body.username;
-const userID =  IDcreator.ID(userName);
 
-User.findOne({username:userName},(err,result)=>{
-  if(err){
-    console.log(err);
-  }else{
-    if(!result){
-      // create a new user
-      const user = new User({
-        username: userName,
-        _id:userID
-      });
-      user.save();
-      res.json({username:userName, _id:userID});
-    }else{
-      // show a message "Username already taken"
-      res.json("Username already taken");
-    }
-  }
-});
 
-});
-
-app.post("/api/exercise/add",(req,res)=>{
-  // add exercise info
-let date = req.body.date ? moment(req.body.date) : moment();
-
-  const userID = req.body.userId;
-  const userDescription = req.body.description;
-  const userDuration = parseInt(req.body.duration);
-  const userDate = req.body.date;
-
-  User.findOne({_id:userID},(err,result)=>{
-    if(err) throw err;
-    //check the user is found or not 
-    if(!result){
-      res.json("The user you were looking for was not found, check your user ID");
-    }else{
-      result.log.push({
-        description : userDescription,
-        duration : userDuration,
-        date : userDate || Date.now()
-      });
-      result.save();
-      res.json(result);
-
-      };
-    }
-  );
-});
-
-app.get("/api/exercise/log",(req,res)=>{
-  // post request for full exercise log
-  const userId = req.query.userId;
-  const from = req.query.from;
-  const to = req.query.to;
-  const limit = req.query.limit; 
-
-  User.findOne({_id:userId},(err,foundUser)=>{
-    if(err) throw err;
-    // Exercise Count
-    const count = foundUser.log.length;
-    //Check user is exist or not
-    if(!foundUser){
-      res.json("The user you were looking for was not found, check your user ID");
-    }else{
-      // if time interval is blank
-      if(!from && !to){
-        //if limit is blank or not
-        if(!limit){
-          res.json({
-            "_id":userId,
-            "username":foundUser.username,
-            "count":count,
-            "log": foundUser.log.map(e => e = {
-              description: e.description,
-              duration: e.duration,
-              date: e.date
-            })
-          });
-        }else{
-          res.json({
-            "_id":userId,
-            "username":foundUser.username,
-            "count":count,
-            "log": foundUser.log.map(e => e = {
-              description: e.description,
-              duration: e.duration,
-              date: e.date
-            }).slice(limit)
-          });
-        }
-      // time interval is not blank  
-      }else{
-
-        //if limit is blank or not
-        if(!limit){
-          res.json({
-            "_id":userId,
-            "username":foundUser.username,
-            "from":from,
-          "to":to,
-            "count":count,
-            "log": foundUser.log.map(e => e = {
-              description: e.description,
-              duration: e.duration,
-              date: e.date
-            })
-          });
-        }else{
-          res.json({
-            "_id":userId,
-            "username":foundUser.username,
-            "from":from,
-          "to":to,
-            "count":count,
-            "log": foundUser.log.map(e => e = {
-              description: e.description,
-              duration: e.duration,
-              date: e.date
-            }).slice(limit)
-          });
-        }
-      }
-    }
-  });
-  
-});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
-
-/*
-{_id:ID, username: USERNAME, from:DATE, to:DATE, count:NUMBERofExercise, log:[EXERCISE LOG]}
-
-Not: Try to split exercise and user schema
-
-*/
